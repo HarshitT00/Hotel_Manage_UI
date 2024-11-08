8c4964a3128f4c2f862b72dc5f62ca6d3a617e93
HarshitT00 <htripathi2003@gmail.com>
2024-11-08 10:26:56 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/hotels" element={<List/>}/>
-        <Route path="/hotels/:id" element={<Hotel/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
+        <Route path="hotel-manage-ui.onrender.com/" element={<Home/>}/>
+        <Route path="hotel-manage-ui.onrender.com/hotels" element={<List/>}/>
+        <Route path="hotel-manage-ui.onrender.com/hotels/:id" element={<Hotel/>}/>
+        <Route path="hotel-manage-ui.onrender.com/login" element={<Login/>}/>
+        <Route path="hotel-manage-ui.onrender.com/register" element={<Register/>}/>
       </Routes>
     </BrowserRouter>
   );
